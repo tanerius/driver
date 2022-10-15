0825354306433143303611a36ee8bc637271d477
Taner Selim <tanerius@gmail.com>
2022-10-15 15:15:58 +0200

    Cas 4

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public bool isPlayer1;
+    public float speed;
+    public Rigidbody2D rb;
+    public float movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPlayer1)
+        {
+            movement = Input.GetAxisRaw("Vertical");
+        }
+        else
+        {
+            movement = Input.GetAxisRaw("Vertical2");
+        }
+
+        rb.velocity = new Vector2(rb.velocity.x, movement * speed);
+    }
+}
